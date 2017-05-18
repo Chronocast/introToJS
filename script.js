a3896ba6f536ce1b213189971c4da53b4469ea84
Nate Hascup <nshascup@gmail.com>
2017-05-17 20:16:55 -0500

    Finished #2 FizzBuzz

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-document.body.innerHTML = "<h2>BuzzFizz</h2>";
 
-console.log("Testing");
+/*
+ *
+ *Author: Nate Hascup
+ *Date: 05/17/17
+ *IT 328: Intro to JavaScript Assignment
+ *
+ */
+
+document.body.innerHTML = "<h1>Introduction to JavaScript</h1>";
+
+document.body.innerHTML = "<h2>FizzBuzz</h2>";
+
+console.log("Introduction to JavaScript");
+
+//print 1 - 100, but replace with word for %3,%5,or %3 & %5
+for (var number = 1; number < 101; number += 1)
+    //replace with FizzBuzz if %3 && %5 true
+    if (number % 3 == 0 && number % 5 == 0)
+        console.log("FizzBuzz");
+    //replace with Fizz if %3
+    else if (number % 3 == 0)
+        console.log("Fizz");
+    //replace with Buzz if %5 true
+    else if (number % 5 == 0)
+        console.log("Buzz");
+    //print number since it is in-divisible
+    else
+        console.log(number);
+        
